18eb6a3d4d8c3f943cc67b5d397bbdab8fd6ca99
Alexsandro Isaac Ramos <alexsandro.r@gmail.com>
2023-09-24 00:08:52 -0300

    header e search ok

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', context={'name': 'Alexsandro', })
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Hotel Flórida',
+    })
